8a9c9661c94cb8b43352046e0e09e60ed7fa9d0f
Shivendra Singh <shivendrapaliwal99990@gmail.com>
2023-02-13 22:46:32 +0530

    portfolio

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -5,10 +5,18 @@ import Navbar from "../Navbar/Navbar";
 import Projects from "../Projects/Projects";
 import Skills from "../Skills/Skills";
 
+
 function Home() {
   return (
     <div>
       <Navbar />
+
+      <div id="home" style={{border:"1px solid red",height:"50vh"}} >
+           <h1>Home</h1>
+           <div>
+             <img src={'C:\Users\shiva\OneDrive\Desktop\Portfolio\my_protfolio\src\Images\My_Pic.jpg'} />
+           </div>
+     </div>
 
       <div>
        <About/>
